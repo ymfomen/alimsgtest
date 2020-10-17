f6e4943c10f6f3c6d438e19a601810ebf4f5d8a7
OMEN <ymfomen@qq.com>
2020-10-18 02:23:04 +0800

    判断是否为电信用户

--- src/main/java/com/ymfomen/alimsg/AlimsgApplication.java
+++ src/main/java/com/ymfomen/alimsg/AlimsgApplication.java
@@ -1,9 +1,13 @@
 package com.ymfomen.alimsg;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
 
 @SpringBootApplication
+@EntityScan(basePackageClasses = {AlimsgApplication.class})
 public class AlimsgApplication {
 
 	public static void main(String[] args) {
