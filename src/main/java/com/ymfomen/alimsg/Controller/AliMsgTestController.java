a4874a6cd14f27f85cf597ee05a72188b89fe6cc
ymfomen <ymfomen@qq.com>
2020-10-18 14:50:03 +0800

    将验证码存放至session中 设置5分钟失效时间

--- src/main/java/com/ymfomen/alimsg/Controller/AliMsgTestController.java
+++ src/main/java/com/ymfomen/alimsg/Controller/AliMsgTestController.java
@@ -1,4 +1,5 @@
 package com.ymfomen.alimsg.Controller;
+
 import com.ymfomen.alimsg.Utli.AliMsgUtil;
 import com.ymfomen.alimsg.Utli.OperatorJudgeUtil;
 import org.springframework.beans.factory.annotation.Value;
@@ -6,8 +7,12 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.util.Map;
+import java.util.Timer;
+import java.util.TimerTask;
 
 /*
 pom.xml
@@ -19,41 +24,52 @@ pom.xml
 */
 @RestController
 @RequestMapping(value = "/indexMsg")
-@PropertySource(value = {"classpath:ali/ali.properties"}, encoding="UTF-8")
+@PropertySource(value = {"classpath:ali/ali.properties"}, encoding = "UTF-8")
 public class AliMsgTestController {
     @Value("${ali.AccessKeyID}")
-    private  String AccessKeyID;
+    private String AccessKeyID;
     @Value("${ali.AccessKeySecret}")
-    private  String AccessKeySecret;
+    private String AccessKeySecret;
     @Value("${ali.SignName}")
-    private  String SignName;
+    private String SignName;
     @Value("${ali.TemplateCode}")
-    private  String TemplateCode;
+    private String TemplateCode;
     @Value("${ali.RegionId}")
-    private  String RegionId;
+    private String RegionId;
     @Value("${ali.SysDomain}")
-    private  String sysDomain;
+    private String sysDomain;
     @Value("${ali.SysVersion}")
-    private  String sysVersion;
+    private String sysVersion;
     @Value("${ali.SendSms}")
-    private  String sendSms;
-    @RequestMapping(value = "/AliSendSms",method = RequestMethod.POST,produces = {"application/json"})
-    public String AliSendSms(HttpServletRequest request){
+    private String sendSms;
+
+    @RequestMapping(value = "/AliSendSms", method = RequestMethod.POST, produces = {"application/json"})
+    public String AliSendSms(HttpServletRequest request, HttpSession session) {
         String phoneNumbers = request.getParameter("PhoneNumbers");
         String operator = OperatorJudgeUtil.checkOperator(phoneNumbers);
-        if(!operator.equals("telecom")) {
+        if (!operator.equals("telecom")) {
             Map<String, String> map = AliMsgUtil.SendSmsUtil(RegionId, AccessKeyID, AccessKeySecret,
                     sysDomain, sysVersion, sendSms,
                     SignName, TemplateCode, phoneNumbers);
             String data = map.get("Data");
             String code = map.get("code");
             if (data.equals("ok")) {
-                request.setAttribute("code",code);
+                session.setAttribute(phoneNumbers, code);
+                //TimerTask实现5分钟后从session中删除checkCode
+                final Timer timer = new Timer();
+                timer.schedule(new TimerTask() {
+                    @Override
+                    public void run() {
+                        session.removeAttribute(phoneNumbers);
+                        System.out.println(phoneNumbers + "删除成功");
+                        timer.cancel();
+                    }
+                }, 5 * 60 * 1000);
                 return "index";
             } else {
                 return "indexDx";
             }
-        }else{
+        } else {
             return "该号码属于电信用户";
         }
     }
